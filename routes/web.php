c23d43441248e561354c3e1b720131d4c3a1347f
Ali Reda <alyredagomaa@gmail.com>
2024-05-22 13:30:18 +0300

    add bootstrap ui with auth with jquery

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
